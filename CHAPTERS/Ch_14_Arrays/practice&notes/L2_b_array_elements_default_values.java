7e8acadcdbb32ba5cb2cd25d420f917a80ffd5cd
akash-chandel-jnp <akashsingh12345@gmail.com>
2023-01-17 20:48:06 +0530

    Added default value of primitive in an array

--- CHAPTERS/Ch_14_Arrays/practice&notes/L2_b_array_elements_default_values.java
+++ CHAPTERS/Ch_14_Arrays/practice&notes/L2_b_array_elements_default_values.java
@@ -2,13 +2,41 @@ import javax.naming.InterruptedNamingException;
 
 public class L2_b_array_elements_default_values{
     public static void main(String[] args) {
-        int marks[] = new int[10]; // default value is 0
-        String names[] = new String[10]; // default value is null
+        byte marks_byte[] = new byte[10]; // default value is 0
+        System.out.println("byte : " + marks_byte[0]);
 
-        System.out.println(marks[0]);
-        System.out.println(names[0]);
+        short marks_short[] = new short[10]; // default value is 0
+        System.out.println("short : " + marks_short[0]);
+
+        int marks[] = new int[10]; // default value is 0
+        System.out.println("int : " + marks[0]);
+        
+        long marks_long[] = new long[10]; // default value is 0
+        System.out.println("long : " + marks_long[0]);
+
+        float marks_float[] = new float[10]; // default value is 0
+        System.out.println("float : " + marks_float[0]);
+
+        double marks_double[] = new double[10]; // default value is 0
+        System.out.println("double: " + marks_double[0]);
+        
+
+
+
+        char marks_char[] = new char[10]; // default value is 0
+        System.out.println("char : " + marks_char[0]);
+        
+        boolean true_false[] = new boolean[10];
+        System.out.println("boolean : " + true_false[0]);
+
         Integer marks2[] = new Integer[10]; // default value is null
-        System.out.println(marks2[0]);
+        System.out.println("Integer : " + marks2[0]);
+
+        String names[] = new String[10]; // default value is null
+        System.out.println("String : "+ names[0]);
+
+
+
 
     }
 
